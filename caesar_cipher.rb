f99ab469b80352290de8cb08b8260241dfcb6fbf
quasi99 <eszquasar@gmail.com>
2021-04-26 21:48:18 -0300

    finish prop

---
+++ caesar_cipher.rb
@@ -0,0 +1,18 @@
+#caesar_cipher demo
+
+def caesar_cipher(string, number)
+	result = ""
+  string.each_char do |letter|
+    op = letter.ord < 91 ? 65 : 97
+    if letter.ord.between?(65, 90) || letter.ord.between?(97, 122)
+      trasnform = (((letter.ord - op) + number) % 26) + op
+      result += trasnform.chr
+    else
+      result += letter
+    end
+  end
+  result
+end
+
+
+p caesar_cipher("What a string!", 5)
